1d801cc15c9b1439701ece79b7b1286de90d3d20
Arek <Arek@AREK-KOMPUTER>
2019-01-07 13:48:35 +0100

    Added Axe

---
+++ Assets/Scripts/Battle/Effects/AxeShootingEffect.cs
@@ -0,0 +1,11 @@
+﻿using Assets.Scripts.Units;
+
+namespace Assets.Scripts.Battle.Effects
+{
+    public class AxeShootingEffect : ProjectileShootingEffect
+    {
+        public AxeShootingEffect() : base(2, ProjectileType.Axe)
+        {
+        }
+    }
+}
